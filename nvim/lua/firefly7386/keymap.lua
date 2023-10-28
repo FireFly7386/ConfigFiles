7f38eb6057f8ad06577bd349726e7029774894f6
FireFly7386 <123244840+FireFly7386@users.noreply.github.com>
2023-10-28 15:28:59 +0200

    began with keymapping

---
+++ nvim/lua/firefly7386/keymap.lua
@@ -0,0 +1,16 @@
+local opts = { noremap = true, silent = true }
+
+local term_opts = { silent = true }
+
+local keymap = vim.api.nvim_set_keymap
+
+keymap("", "<Space>", "<Nop>", opts)
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+keymap("n", "<C-h>", "<C-w>h", opts)
+keymap("n", "<C-j>", "<C-w>j", opts)
+keymap("n", "<C-k>", "<C-w>k", opts)
+keymap("n", "<C-l>", "<C-w>l", opts)
+
+keymap("n", "<leader>e", ":Lex 30<cr>", opts)
