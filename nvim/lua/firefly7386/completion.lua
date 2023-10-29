53e78284a7e9e52c497d23c4566ace8bc9ff47f2
FireFly7386 <123244840+FireFly7386@users.noreply.github.com>
2023-10-29 10:47:49 +0100

    Update completion.lua

--- nvim/lua/firefly7386/completion.lua
+++ nvim/lua/firefly7386/completion.lua
@@ -15,6 +15,34 @@ local check_backspace = function()
   return col == 0 or vim.fn.getline("."):sub(col, col):match "%s"
 end
 
+local icons = {
+  Text = "󰊄",
+  Method = "m",
+  Function = "󰊕",
+  Constructor = "",
+  Field = "",
+  Variable = "󰫧",
+  Class = "",
+  Interface = "",
+  Module = "",
+  Property = "",
+  Unit = "",
+  Value = "",
+  Enum = "",
+  Keyword = "󰌆",
+  Snippet = "",
+  Color = "",
+  File = "",
+  Reference = "",
+  Folder = "",
+  EnumMember = "",
+  Constant = "",
+  Struct = "",
+  Event = "",
+  Operator = "",
+  TypeParameter = "󰉺",
+}
+
 cmp.setup {
   snippet = {
     expand = function(args)
@@ -66,6 +94,7 @@ cmp.setup {
   formatting = {
     fields = { "kind", "abbr", "menu" },
     format = function(entry, vim_item)
+      vim_item.kind = string.format("%s", icons[vim_item.kind])
       vim_item.menu = ({
         luasnip = "[Snippet]",
         buffer = "[Buffer]",
